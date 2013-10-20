f9e839334e599f3d63d2dccff8fa2bbaaca4780f
robjean9 <robjean9@hotmail.com>
2013-10-20 20:55:47 -0200

    20/10/2013 -- ServerController
    Criação do código de conexão e criação de um server local.

---
+++ Assets/Scripts/ServerController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ServerController : MonoBehaviour {
+	
+	public string ipToConnect;
+	public int portToConnect;
+	public int numberPlayers;
+	
+
+	// Use this for initialization
+	void Start () {
+		ipToConnect = Network.player.ipAddress;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		
+		
+	}
+	
+	void StartServer(){
+		if(Network.peerType == NetworkPeerType.Disconnected){
+		Debug.Log(Network.InitializeServer(numberPlayers, portToConnect, false));
+				
+		}
+	}
+	void ConnectToServer(){
+		if(Network.peerType == NetworkPeerType.Disconnected){
+			Debug.Log(Network.Connect(ipToConnect, portToConnect));	
+		}	
+	}
+	
+	void OnGUI(){
+		if(Network.peerType == NetworkPeerType.Disconnected){
+			if(GUI.Button(new Rect(Screen.width/2-100,10,200,30), "Iniciar Servidor")){
+				StartServer();	
+			}
+			ipToConnect = GUI.TextField(new Rect(Screen.width/2-100, 40, 200, 30), ipToConnect);
+			if(GUI.Button(new Rect(Screen.width/2-100,70, 200, 30), "Conectar ao Servidor")){
+				ConnectToServer();	
+			}
+		}
+	}
+}
